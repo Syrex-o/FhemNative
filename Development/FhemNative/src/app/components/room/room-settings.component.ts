4bed3c03c4ff13ac4de8c8f750b07fbdd085613e
Syrex-o <alberthenkel.ah@gmail.com>
2019-09-23 11:20:50 +0200

    removed unused code

--- Development/FhemNative/src/app/components/room/room-settings.component.ts
+++ Development/FhemNative/src/app/components/room/room-settings.component.ts
@@ -492,16 +492,6 @@ export class SettingsRoomComponent {
 		}
 	}
 
-	private arrayBufferToBase64( buffer ) {
-		let binary = '';
-	    let bytes = new Uint8Array( buffer );
-	    let len = bytes.byteLength;
-	    for (let i = 0; i < len; i++) {
-	        binary += String.fromCharCode( bytes[ i ] );
-	    }
-	    return window.btoa( binary );
-	}
-
 	private evaluateJsonImport(rawData) {
 		// disconnect from fhem
 		this.fhem.noReconnect = true;
