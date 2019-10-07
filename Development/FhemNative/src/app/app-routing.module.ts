0e2875d9539c5e36187e04ce9c45a6c7ecbb357b
Syrex-o <alberthenkel.ah@gmail.com>
2019-10-07 11:38:38 +0200

    added language values

--- Development/FhemNative/src/app/app-routing.module.ts
+++ Development/FhemNative/src/app/app-routing.module.ts
@@ -2,15 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-	{
-		path: '',
-		redirectTo: '/',
-		pathMatch: 'full'
-	},
-	{
-		path: '**',
-		redirectTo: '/'
-	}
+	{ path: '', redirectTo: '/', pathMatch: 'full' },
+	{ path: '**', redirectTo: '/'}
 ];
 
 @NgModule({
