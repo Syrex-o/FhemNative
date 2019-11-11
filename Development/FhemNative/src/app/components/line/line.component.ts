83fd0df1af811ee4388d2d8c6808f496dce89939
Syrex-o <alberthenkel.ah@gmail.com>
2019-11-11 08:29:27 +0100

    added features to line component

--- Development/FhemNative/src/app/components/line/line.component.ts
+++ Development/FhemNative/src/app/components/line/line.component.ts
@@ -15,8 +15,32 @@ import { SettingsService } from '../../services/settings.service';
 			minimumHeight="20"
 			id="{{ID}}"
 			[ngStyle]="{'width': width, 'height': height, 'top': top, 'left': left, 'z-index': zIndex}">
-			<div *ngIf="arr_data_orientation[0] == 'horizontal'" class="line-item horizontal" [ngStyle]="{'height': data_height+'px', 'background': style_color}"></div>
-			<div *ngIf="arr_data_orientation[0] == 'vertical'" class="line-item vertical" [ngStyle]="{'width': data_height+'px', 'background': style_color}"></div>
+			<div class="line-container">
+				<div 
+					*ngIf="arr_data_orientation[0] === 'horizontal'" 
+					class="line-item horizontal"
+					[ngStyle]="{
+						'height': data_curve_percentage === '0' ? data_height+'px' : '100%', 
+						'background': data_curve_percentage === '0' ? style_color : 'transparent', 
+						'transform': 'translate3d(0, -50%, 0) rotate('+data_rotation+'deg)',
+						'border-left': data_curve_percentage !== '0' ? data_height+'px solid '+style_color : '0px',
+						'border-top': data_curve_percentage !== '0' ? data_height+'px solid '+style_color : '0px',
+						'border-top-left-radius': data_curve_percentage+'%'
+					}">
+				</div>
+				<div 
+					*ngIf="arr_data_orientation[0] === 'vertical'" 
+					class="line-item vertical"
+					[ngStyle]="{
+						'width': data_curve_percentage === '0' ? data_height+'px' : '100%', 
+						'background': data_curve_percentage === '0' ? style_color : 'transparent', 
+						'transform': 'translate3d(-50%, 0, 0) rotate('+data_rotation+'deg)',
+						'border-right': data_curve_percentage !== '0' ? data_height+'px solid '+style_color : '0px',
+						'border-bottom': data_curve_percentage !== '0' ? data_height+'px solid '+style_color : '0px',
+						'border-bottom-right-radius': data_curve_percentage+'%'
+					}">
+				</div>
+			</div>
 		</div>
 	`,
 	styles: [`
@@ -25,19 +49,23 @@ import { SettingsService } from '../../services/settings.service';
     		width: 30px;
     		height: 30px;
 		}
+		.line-container{
+			width: 100%;
+			height: 100%;
+			overflow: hidden;
+			position: relative;
+		}
 		.line-item{
 			position: absolute;
 		}
 		.horizontal{
 			width: 100%;
 			top: 50%;
-			transform: translate3d(0-50%,0);
 			left: 0;
 		}
 		.vertical{
 			height: 100%;
 			left: 50%;
-			transform: translate3d(-50%,0,0);
 			top: 0;
 		}
 	`]
@@ -48,6 +76,8 @@ export class LineComponent {
 
 	@Input() arr_data_orientation: string|string[];
 	@Input() data_height: string;
+	@Input() data_rotation: string;
+	@Input() data_curve_percentage: string;
 
 	// Styling
 	@Input() style_color: string;
@@ -70,6 +100,8 @@ export class LineComponent {
 			inputs: [
 				{variable: 'arr_data_orientation', default: 'vertical,horizontal'},
 				{variable: 'data_height', default: '2'},
+				{variable: 'data_rotation', default: '0'},
+				{variable: 'data_curve_percentage', default: '0'},
 				{variable: 'style_color', default: '#86d993'}
 			],
 			dimensions: {minX: 30, minY: 30}
