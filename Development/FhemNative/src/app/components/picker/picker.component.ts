9ca45ee28613a1bf29a793b4d109fd1bf93c0a82
Syrex-o <alberthenkel.ah@gmail.com>
2019-11-11 08:30:15 +0100

    removed unused stuff from picker

--- Development/FhemNative/src/app/components/picker/picker.component.ts
+++ Development/FhemNative/src/app/components/picker/picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
