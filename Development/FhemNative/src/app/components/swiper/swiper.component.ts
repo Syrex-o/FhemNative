a141a9a76fa08537f173f6ce087479bede4fb33c
Syrex-o <alberthenkel.ah@gmail.com>
2019-11-22 15:51:37 +0100

    fixed swiper + new grid loader

--- Development/FhemNative/src/app/components/swiper/swiper.component.ts
+++ Development/FhemNative/src/app/components/swiper/swiper.component.ts
@@ -42,13 +42,16 @@ import { HelperService } from '../../services/helper.service';
 				    </ion-slides>
 				</div>
 			</fhem-container>
+			<button matRipple [matRippleColor]="'#d4d4d480'" class="btn-round" *ngIf="settings.modes.roomEdit && this.editingEnabled" (click)="swiperEditMode()">
+			    <ion-icon class="edit" name="create"></ion-icon>
+			</button>
 		</div>
 	`,
 	styles: [`
 		.swiper{
 			position: absolute;
-    		width: 100px;
-    		height: 50px;
+    		width: 130px;
+    		height: 130px;
 		}
 		p{
 			text-align: center;
@@ -64,6 +67,27 @@ import { HelperService } from '../../services/helper.service';
 		.dark p{
 			color: var(--dark-p);
 		}
+		.btn-round{
+			position: absolute;
+			top: 5px;
+			right: 8px;
+			width: 45px;
+		    height: 45px;
+		    border-radius: 50%;
+		    border: none;
+		    box-shadow: 0px 4px 10px 0px rgba(0,0,0,0.3);
+		    z-index: 101;
+		}
+		.btn-round:focus{
+			outline: 0px;
+		}
+		.btn-round ion-icon{
+			color: var(--btn-blue);
+			font-size: 25px;
+		}
+		.dark .btn-round{
+			background: var(--dark-bg);
+		}
 	`],
 })
 
@@ -77,11 +101,6 @@ export class SwiperComponent implements OnInit, OnDestroy {
 		private helper: HelperService) {}
 	@ViewChild( IonSlides, { static: true } ) slides: IonSlides;
 	@ViewChildren('container', { read: ViewContainerRef }) containers: QueryList<ViewContainerRef>;
-
-	// mouse positions
-	private currentMouse = {x: 0, y: 0};
-	private startMouse = {x: 0, y: 0};
-	private sliderIndex = 0;
 
 	// Component ID
 	@Input() ID: number;
@@ -109,12 +128,13 @@ export class SwiperComponent implements OnInit, OnDestroy {
 	@Input() zIndex: number;
 
 	public pages: Array<any>;
+	private currentContainer:any;
 
 	// Subscription to room editor
 	private modeSubscriber: Subscription;
 
 	// custom edit mode, to disable resize, if swiper editing is active
-	public editingEnabled = false;
+	public editingEnabled: boolean = false;
 
 	static getSettings() {
 		return {
@@ -131,7 +151,7 @@ export class SwiperComponent implements OnInit, OnDestroy {
 				{variable: 'style_headerColor', default: '#434E5D'},
 				{variable: 'style_backgroundColor', default: '#58677C'}
 			],
-			dimensions: {minX: 100, minY: 50}
+			dimensions: {minX: 60, minY: 60}
 		};
 	}
 
@@ -139,39 +159,30 @@ export class SwiperComponent implements OnInit, OnDestroy {
 	@HostListener('document:mousedown', ['$event'])
 	onMouseDown(e) {
 		if (this.settings.modes.roomEdit) {
-			this.startMouse = {
-				x: e.pageX || (e.touches ? e.touches[0].clientX : 0),
-				y: e.pageY || (e.touches ? e.touches[0].clientY : 0)
-			};
 			// identify the click object
 			if (!this.editingEnabled && (e.target.className.indexOf('content') !== -1 || (e.target.className.indexOf('overlay-move') !== -1 && !this.ref.nativeElement.contains(e.target))) ) {
-				this.roomEditMode();
+				if(this.structure.canEdit(this.ID)){
+					this.editingEnabled = true;
+					// change edit ID back to relevant
+					if(e.target.className.indexOf('popup') !== -1){
+						// switch back to popup
+						const id = e.target.id.replace('popup_', '');
+						this.settings.modeSub.next({roomEditFrom: id});
+					}else{
+						this.settings.modeSub.next({roomEditFrom: this.structure.getCurrentRoom().item.ID});
+					}
+					this.removeHelpers();
+				}
 			}
 			// identify if swiping should be allowed or not
-			if (!this.editingEnabled && e.target.className.match(/(grid|line)/)) {
-				this.slides.lockSwipes(false);
-			} else {
+			if(this.structure.canEdit(this.ID)){
+				if (!this.editingEnabled && e.target.className.match(/(grid|line)/)) {
+					this.slides.lockSwipes(false);
+				}else{
+					this.slides.lockSwipes(true);
+				}
+			}else{
 				this.slides.lockSwipes(true);
-			}
-		}
-	}
-
-	@HostListener('document:mouseup', ['$event'])
-	@HostListener('document:touchend', ['$event'])
-	onClick(e) {
-		if (this.settings.modes.roomEdit) {
-			this.currentMouse = {
-				x: e.pageX || (e.changedTouches ? e.changedTouches[0].clientX : 0),
-				y: e.pageY || (e.changedTouches ? e.changedTouches[0].clientY : 0)
-			};
-			// identify the click object
-			if (this.editingEnabled && this.ref.nativeElement.contains(e.target)) {
-				// respect double click event
-				setTimeout(() => {
-					if (!this.ref.nativeElement.firstChild.classList.contains('double-click') && Math.abs(this.startMouse.x - this.currentMouse.x) < 5 && Math.abs(this.startMouse.y - this.currentMouse.y) < 5) {
-						this.swiperEditMode();
-					}
-				}, 300);
 			}
 		}
 	}
@@ -200,11 +211,13 @@ export class SwiperComponent implements OnInit, OnDestroy {
 			// loading swiper components
 			this.looper((i) => {
 				this.createComponent.loadRoomComponents(
-					this.structure.roomComponents(this.containers[i]),
+					this.structure.getComponentContainer(this.containers[i]),
 					this.containers[i],
 					false
 				);
 			});
+
+			this.currentContainer = this.containers[0];
 
 			// getting initial roomEdit state
 			if (this.settings.modes.roomEdit) {
@@ -212,15 +225,16 @@ export class SwiperComponent implements OnInit, OnDestroy {
 			}
 			// detecting mode changes in room edit
 			this.modeSubscriber = this.settings.modeSub.subscribe(next => {
-				if (this.settings.modes.roomEdit) {
-					this.editingEnabled = true;
-				} else {
-					this.editingEnabled = false;
-					this.slides.lockSwipes(false);
-					// removing grid on all swiper views
-					this.looper((i) => {
-						this.createComponent.removeSingleComponent('GridComponent', this.containers[i]);
-					});
+				if(next.hasOwnProperty('roomEdit') || next.hasOwnProperty('roomEditFrom')){
+					if(this.settings.modes.roomEdit){
+						if(this.structure.canEdit(this.ID)){
+							this.editingEnabled = true;
+						}
+					}else{
+						this.editingEnabled = false;
+						this.slides.lockSwipes(false);
+						this.removeHelpers();
+					}
 				}
 			});
 			// getting the spawn room ID, to remove components after OnDestroy
@@ -234,57 +248,49 @@ export class SwiperComponent implements OnInit, OnDestroy {
 		}
 	}
 
+	public swiperEditMode(){
+		this.settings.modeSub.next({roomEditFrom: this.ID});
+		// create helpers
+		this.createHelpers();
+		// remove edit button
+		this.editingEnabled = false;
+		this.slides.lockSwipes(true);
+	}
+
+	private createHelpers(){
+		if(this.structure.canEdit(this.ID)){
+			// grid
+			this.looper((i)=>{
+				this.createComponent.createSingleComponent('GridComponent', (this.containers['_results'] ? this.containers['_results'][i] : this.containers[i]), {
+					container: (this.containers['_results'] ? this.containers['_results'][i] : this.containers[i])
+				});
+			});
+			// create component
+			this.createComponent.createSingleComponent('CreateComponentComponent', this.createComponent.currentRoomContainer, {
+				container: this.currentContainer
+			});
+		}else{
+			this.removeHelpers();
+		}
+	}
+
+	private removeHelpers(){
+		this.looper((i)=>{
+			this.createComponent.removeSingleComponent('GridComponent', (this.containers['_results'] ? this.containers['_results'][i] : this.containers[i]) );
+		});
+		this.createComponent.removeSingleComponent('CreateComponentComponent', this.currentContainer);
+	}
+
 	public getSliderIndex() {
 		// getting the current index
 		this.slides.getActiveIndex().then((index) => {
-			this.sliderIndex = index - 1;
-			if (this.editingEnabled) {
-				this.createComponent.removeSingleComponent('EditComponentComponent', this.createComponent.currentRoomContainer);
+			this.currentContainer = this.containers['_results'] ? this.containers['_results'][index] : this.containers[index];
+			// build new create component for current container
+			if(this.settings.modes.roomEdit && !this.editingEnabled){
 				this.createComponent.createSingleComponent('CreateComponentComponent', this.createComponent.currentRoomContainer, {
-					container: this.containers[index]
+					container: this.currentContainer
 				});
 			}
-		});
-	}
-
-	private roomEditMode() {
-		// switch back to room edit mode
-		this.editingEnabled = true;
-		// remove grid on outside click and switch back to normal edit mode
-		// removing createcomponent for swiper
-		this.createComponent.removeSingleComponent('EditComponentComponent', this.createComponent.currentRoomContainer);
-		// adding createcomponent for room
-		this.createComponent.createSingleComponent('CreateComponentComponent', this.createComponent.currentRoomContainer, {
-			container: this.createComponent.currentRoomContainer
-		});
-		// removing grid on all swiper views
-		this.looper((i) => {
-			this.createComponent.removeSingleComponent('GridComponent', this.containers[i]);
-		});
-		// adding grid back to room
-		this.createComponent.createSingleComponent('GridComponent', this.createComponent.currentRoomContainer, {
-			container: this.createComponent.currentRoomContainer
-		});
-	}
-
-	private swiperEditMode() {
-		// switch to swiper edit mode
-		this.editingEnabled = false;
-		this.slides.lockSwipes(true);
-		// removing grid on room
-		this.createComponent.removeSingleComponent('GridComponent', this.createComponent.currentRoomContainer);
-		// removing createComponent
-		this.createComponent.removeSingleComponent('CreateComponentComponent', this.createComponent.currentRoomContainer);
-
-		// building grid on all slider views
-		this.looper((i) => {
-			this.createComponent.createSingleComponent('GridComponent', this.containers[i], {
-				container: this.containers[i]
-			});
-		});
-
-		this.createComponent.createSingleComponent('CreateComponentComponent', this.createComponent.currentRoomContainer, {
-			container: this.containers[this.sliderIndex]
 		});
 	}
 
@@ -295,10 +301,14 @@ export class SwiperComponent implements OnInit, OnDestroy {
 	private removeFhemComponents() {
 		// removing fhem components in slider from view
 		this.looper((i) => {
-			const sliderComponents = this.helper.find(this.structure.rooms[this.roomID].components, 'ID', this.ID).item.attributes.components[i];
+			const sliderComponents = this.structure.getComponentContainer( (this.containers['_results'] ? this.containers['_results'][i] : this.containers[i]) );
 			if(sliderComponents){
-				for (let j = 0; j < sliderComponents.length; j++) {
-					this.createComponent.removeFhemComponent(sliderComponents[j].ID);
+				sliderComponents.forEach((comp)=>{
+					this.createComponent.removeFhemComponent(comp.ID);
+				});
+				if(this.containers['_results']){
+					this.containers['_results'][i].clear();
+				}else{
 					this.containers[i].clear();
 				}
 			}
