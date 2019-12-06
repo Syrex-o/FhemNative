d42d9d65f12e89b548bfd6a148a936645bdac66c
Syrex-o <alberthenkel.ah@gmail.com>
2019-12-06 10:40:43 +0100

    added function

--- Development/FhemNative/src/app/services/time.service.ts
+++ Development/FhemNative/src/app/services/time.service.ts
@@ -26,7 +26,7 @@ export class TimeService {
 		return h + ':' + m;
 	}
 
-	// return time HH:MM:SS from sec
+	// return time HH:MM:SS from sec if HH > 0
 	public secToTime(sec){
 		let pad = (num, size)=> { return ('000' + num).slice(size * -1); }
 		let t: any = parseFloat(sec).toFixed(3);
@@ -35,6 +35,17 @@ export class TimeService {
 		let s: any = Math.floor(t - m * 60);
 
 		return (pad(h, 2) === '00' ? '' : pad(h, 2) + ':') + pad(m, 2) + ':' + pad(s, 2);
+	}
+
+	// return time HH:MM:SS from sec
+	public secToFullTime(sec){
+		let pad = (num, size)=> { return ('000' + num).slice(size * -1); }
+		let t: any = parseFloat(sec).toFixed(3);
+		let h: any = Math.floor(t / 60 / 60);
+		let m: any = Math.floor(t / 60) % 60;
+		let s: any = Math.floor(t - m * 60);
+
+		return pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2);
 	}
 
 	// returns duration in obj (min, durationTime) from time or minutes
