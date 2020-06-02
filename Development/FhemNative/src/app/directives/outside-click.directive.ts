17d54e1bddf1995be4af0eedb5ed62333ae70e2b
Syrex-o <alberthenkel.ah@gmail.com>
2020-06-02 11:42:39 +0200

    outside click fix

--- Development/FhemNative/src/app/directives/outside-click.directive.ts
+++ Development/FhemNative/src/app/directives/outside-click.directive.ts
@@ -1,29 +1,24 @@
-import { Directive, ElementRef, HostListener, Output, EventEmitter, NgZone, OnInit, OnDestroy } from '@angular/core';
+import { Directive, ElementRef, HostListener, Output, EventEmitter } from '@angular/core';
 
 
 @Directive({ selector: '[outside-click]' })
-export class OutsideClickDirective implements OnInit, OnDestroy {
+export class OutsideClickDirective {
 	@Output() onOutsideClick = new EventEmitter();
 
-	constructor(private ref: ElementRef, private zone: NgZone){}
+	constructor(private ref: ElementRef){}
 
- 	ngOnInit(){
- 		this.zone.runOutsideAngular(()=>{
- 			document.addEventListener('mousedown', this.listener);
- 			document.addEventListener('touchstart', this.listener);
- 		});
- 	}
+ 	@HostListener('document:mousedown', ['$event.target'])
+	@HostListener('document:touchstart', ['$event.target']) 
+	onClick(target) {
+    	if (!this.ref.nativeElement.contains(target)) {
+      		this.onOutsideClick.emit(target);
+    	}
+  	}
 
  	private listener = (e)=>{
  		if(!this.ref.nativeElement.contains(e.target)){
+ 			console.log('hi');
 	 		this.onOutsideClick.emit(e.target);
 	 	}
- 	}
-
- 	ngOnDestroy(){
- 		this.zone.runOutsideAngular(()=>{
- 			document.removeEventListener('mousedown', this.listener);
- 			document.removeEventListener('touchstart', this.listener);
- 		});
  	}
 }
