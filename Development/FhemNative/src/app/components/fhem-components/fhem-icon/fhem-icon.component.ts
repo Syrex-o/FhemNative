588d5ba052ac26057cc82316de73701ecaddc747
Syrex-o <alberthenkel.ah@gmail.com>
2020-06-02 11:40:21 +0200

    icon min max colors

--- Development/FhemNative/src/app/components/fhem-components/fhem-icon/fhem-icon.component.ts
+++ Development/FhemNative/src/app/components/fhem-components/fhem-icon/fhem-icon.component.ts
@@ -18,6 +18,10 @@ export class FhemIconComponent implements OnInit, OnDestroy {
 	@Input() data_indicatorReading: string;
 	@Input() data_getOn: string;
 	@Input() data_getOff: string;
+
+	@Input() data_min: string;
+	@Input() data_max: string;
+
 	@Input() bool_data_showIndocator: boolean;
 
 	// Icon
@@ -29,6 +33,9 @@ export class FhemIconComponent implements OnInit, OnDestroy {
 	@Input() style_iconColorOff: string;
 	@Input() style_indicatorColor: string;
 	@Input() style_indicatorBackgroundColor: string;
+
+	@Input() style_minColor: string;
+	@Input() style_maxColor: string;	
 
 	// position information
 	@Input() width: string;
@@ -56,6 +63,27 @@ export class FhemIconComponent implements OnInit, OnDestroy {
 		}
 	}
 
+	getValueColor(){
+		// int colors
+		if(!isNaN(this.fhemDevice.readings[this.data_reading].Value)){
+			if(this.data_min !== '' && this.fhemDevice.readings[this.data_reading].Value < parseFloat(this.data_min)){
+				return this.style_minColor;
+			}
+			if(this.data_max !== '' && this.fhemDevice.readings[this.data_reading].Value > parseFloat(this.data_max)){
+				return this.style_maxColor;
+			}
+		}else{
+			if(this.data_min !== '' && this.fhemDevice.readings[this.data_reading].Value === this.data_min){
+				return this.style_minColor;
+			}
+			if(this.data_max !== '' && this.fhemDevice.readings[this.data_reading].Value === this.data_max){
+				return this.style_maxColor;
+			}
+		}
+		// return default state
+		return this.iconState ? this.style_iconColorOn : this.style_iconColorOff;
+	}
+
 	ngOnDestroy(){
 		this.fhem.removeDevice(this.ID);
 	}
@@ -72,13 +100,17 @@ export class FhemIconComponent implements OnInit, OnDestroy {
 				{variable: 'data_indicatorReading', default: ''},
 				{variable: 'data_getOn', default: 'on'},
 				{variable: 'data_getOff', default: 'off'},
+				{variable: 'data_min', default: ''},
+				{variable: 'data_max', default: ''},
+				{variable: 'bool_data_showIndocator', default: false},
 				{variable: 'icon_iconOn', default: 'add-circle'},
 				{variable: 'icon_iconOff', default: 'add-circle'},
 				{variable: 'style_iconColorOn', default: '#86d993'},
 				{variable: 'style_iconColorOff', default: '#86d993'},
 				{variable: 'style_indicatorColor', default: '#86d993'},
 				{variable: 'style_indicatorBackgroundColor', default: '#58677c'},
-				{variable: 'bool_data_showIndocator', default: false},
+				{variable: 'style_minColor', default: '#02adea'},
+				{variable: 'style_maxColor', default: '#fb0a2a'}
 			],
 			dependencies: {
 				data_indicatorReading: { dependOn: 'bool_data_showIndocator', value: true },
