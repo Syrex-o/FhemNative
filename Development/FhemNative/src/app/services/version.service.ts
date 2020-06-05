d0dd59352a32fd8ee9ab045c6d4618c2d8891454
Syrex-o <alberthenkel.ah@gmail.com>
2020-06-05 11:27:54 +0200

    version update

--- Development/FhemNative/src/app/services/version.service.ts
+++ Development/FhemNative/src/app/services/version.service.ts
@@ -16,7 +16,7 @@ export class VersionService {
 	// github repo
 	private repo: string = 'https://api.github.com/repos/Syrex-o/FhemNative/';
 	// current App Version
-	public appVersion: string = '2.5.2';
+	public appVersion: string = '2.5.3';
 
 	constructor(
 		private http: HttpClient,
