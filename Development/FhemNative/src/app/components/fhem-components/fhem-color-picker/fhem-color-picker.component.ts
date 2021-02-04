819b95736a26d7b8ca6c5894865115a6dc1bbba1
Syrex-o <alberthenkel.ah@gmail.com>
2021-02-04 13:39:10 +0100

    additional colorpicker functionality

--- Development/FhemNative/src/app/components/fhem-components/fhem-color-picker/fhem-color-picker.component.ts
+++ Development/FhemNative/src/app/components/fhem-components/fhem-color-picker/fhem-color-picker.component.ts
@@ -9,11 +9,12 @@ import { FhemService } from '../../../services/fhem.service';
 import { SettingsService } from '../../../services/settings.service';
 import { StorageService } from '../../../services/storage.service';
 import { NativeFunctionsService } from '../../../services/native-functions.service';
+import { SelectComponentService } from '../../../services/select-component.service';
 
 @Component({
 	selector: 'fhem-color-picker',
 	templateUrl: './fhem-color-picker.component.html',
-  	styleUrls: ['./fhem-color-picker.component.scss']
+	styleUrls: ['./fhem-color-picker.component.scss']
 })
 export class FhemColorPickerComponent implements OnInit, OnDestroy {
 	private canvas: any;
@@ -39,6 +40,8 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 	@Input() data_borderRadiusBottomLeft: string;
 	@Input() data_borderRadiusBottomRight: string;
 
+	@Input() bool_data_showFavMenu: boolean;
+	@Input() bool_data_usePopup: boolean;
 	@Input() bool_data_customBorder: boolean;
 	@Input() bool_data_updateOnMove: boolean;
 	@Input() bool_data_customAnimation: boolean;
@@ -69,50 +72,62 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 	styles: any = {
 		outerRotation: 90,
 		outerGradient: 'rgb(155, 134, 100)',
-		innerTranslate: 'translate(0,0)'
-	}
+		innerTranslate: 'translate(0,0)',
+		animateInner: true,
+		animateOuter: true
+	};
 
 	@HostListener('mousedown', ['$event', '$event.target'])
 	@HostListener('touchstart', ['$event', '$event.target'])
 	onTouchstart(event, target) {
 		if (target.className.match(/(outer|inner)/)) {
-			// start move
-			if(target.className.match(/inner/)){
-				this.styles.outerRotation = 90;
-			}
+			// get relevant move function
+			let moveFunc: string = target.className.match(/outer/) ? 'rotateOuter' : 'moveInner';
+			// update on movement, based on move function
 			const whileMove = (e) => {
 				e.stopPropagation();
-	        	if (this.fhemDevice) {
-	        		if(target.className.match(/outer/)){
-	        			this.rotateOuter(e);
-	        		}else{
-	        			this.moveInner(e);
-	        		}
-	        	}
-	        }
-	        const endMove = () => {
+				if(this.fhemDevice){
+					this[moveFunc](e);
+				}
+			}
+			const endMove = () => {
 				window.removeEventListener('mousemove', whileMove);
 				window.removeEventListener('touchmove', whileMove);
 
-	   			window.removeEventListener('mouseup', endMove);
-	   			window.removeEventListener('touchend', endMove);
-	   			// send value update
-	   			const color = this.colorSelector('hex', this.arr_data_colorOutput[0], this.color);
-	   			if (this.fhemDevice.readings[this.data_reading].Value !== color) {
-	   				this.sendValue(color);
-	   			}
-	   			this.waitForThreshold = 0;
-	   		}
+				window.removeEventListener('mouseup', endMove);
+				window.removeEventListener('touchend', endMove);
+				// send value update
+				const color = this.colorSelector('hex', this.arr_data_colorOutput[0], this.color);
+				if (this.fhemDevice.readings[this.data_reading].Value !== color) {
+					this.sendValue(color);
+				}
+				this.waitForThreshold = 0;
+				// enable animating again
+				this.styles.animateOuter = true;
+				this.styles.animateInner = true;
+			}
 
-	        window.addEventListener('mousemove', whileMove);
-	  		window.addEventListener('mouseup', endMove);
+			// start move animation blocker
+			if(target.className.match(/outer/)){
+				this.styles.animateInner = false;
+				this.styles.animateOuter = false;
+			}else{
+				this.styles.animateInner = false;
+			}
 
-	  		window.addEventListener('touchmove', whileMove);
-	  		window.addEventListener('touchend', endMove);
+			window.addEventListener('mousemove', whileMove);
+			window.addEventListener('mouseup', endMove);
+
+			window.addEventListener('touchmove', whileMove);
+			window.addEventListener('touchend', endMove);
 		}
 	}
 
 	ngOnInit(){
+		// show canvas, when used outside of pupup
+		if(!this.bool_data_usePopup){
+			this.popupOpened();
+		}
 		this.fhem.getDevice(this.ID, this.data_device, (device)=>{
 			this.getState(device);
 		}).then(device=>{
@@ -121,52 +136,111 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 				this.getOuterGradientColor();
 			}
 		});
+
+		// resize handle
+		this.selectComponent.addHandle(this.ID, 'resize', (dimensions: {[key: string]: number})=>{
+			if(!this.bool_data_usePopup || (this.bool_data_usePopup && this.popupState)){
+				this.width = dimensions.width + 'px';
+				this.height = dimensions.height + 'px';
+				this.drawCanvas();
+			}
+		});
 	}
 
-	private getState(device){
+	private getState(device): void{
 		this.fhemDevice = device;
 		if(device){
 			if(device.readings[this.data_reading]){
 				this.color = this.colorSelector(this.arr_data_colorInput[0], 'hex', device.readings[this.data_reading].Value.toString());
+				// get outer roation angle
+				this.getOuterRotation();
+				// get inner position
+				this.getInnerPosition();
 			}
 		}
 	}
 
-	togglePopup(){
+	togglePopup(): void{
 		this.popupState = !this.popupState;
 	}
 
-	popupOpened(){
-		this.canvas = this.container.nativeElement.querySelector('.canvas');
+	// get size for canvas
+	private getCanvasSize(): number{
+		const container: HTMLElement = this.container.nativeElement;
+		let dim: number = 0;
+
+		if(this.bool_data_showFavMenu){
+			dim = Math.min(
+				container.clientWidth || parseFloat(this.width) * 0.95,
+				container.clientHeight || parseFloat(this.height) * 0.9
+			) -20;
+		}else{
+			dim = Math.min(
+				container.clientWidth || parseFloat(this.width),
+				container.clientHeight || parseFloat(this.height)
+			) -20;
+		}
+
+		return dim;
+	}
+
+	private drawCanvas(): void{
+		if(this.canvas){
+			this.canvas.getContext('2d').clearRect(0, 0, this.canvas.width, this.canvas.height);
+		}
+		this.dimension = this.getCanvasSize();
+		// draw canvas content
+		this.canvas.width = this.dimension * 0.82;
+		this.canvas.height = this.dimension * 0.82;
+
+		const x = this.canvas.width / 2;
+		const y = this.canvas.height / 2;
+		const ctx = this.canvas.getContext('2d');
+		for (let i = 0; i < 360; i += 0.2) {
+			const rad = (i - 45) * (Math.PI) / 180;
+			ctx.strokeStyle = 'hsla(' + i + ', 100%, 50%, 1.0)';
+			ctx.beginPath();
+			ctx.moveTo(x, y);
+			ctx.lineTo(x + x * Math.cos(-rad), y + y * Math.sin(-rad));
+			ctx.stroke();
+		}
+	}
+
+	popupOpened(): void{
 		// assign values
 		setTimeout(()=>{
-			let container = this.container.nativeElement;
-			this.dimension = Math.min( container.clientWidth, container.clientHeight ) - 20;
-
-			// draw canvas content
-			this.canvas.width = this.dimension - 80;
-			this.canvas.height = this.dimension - 80;
-
-			const x = this.canvas.width / 2;
-  			const y = this.canvas.height / 2;
-  			const ctx = this.canvas.getContext('2d');
-
-  			for (let i = 0; i < 360; i += 0.2) {
-            	const rad = (i - 45) * (Math.PI) / 180;
-	            ctx.strokeStyle = 'hsla(' + i + ', 100%, 50%, 1.0)';
-	            ctx.beginPath();
-	            ctx.moveTo(x, y);
-	            ctx.lineTo(x + x * Math.cos(-rad), y + y * Math.sin(-rad));
-	            ctx.stroke();
-	        }
+			// get canvas
+			this.canvas = this.container.nativeElement.querySelector('.canvas');
+			this.drawCanvas();
 		});
 	}
 
-	popupClosed(){
-		this.canvas.getContext('2d').clearRect(0, 0, this.canvas.width, this.canvas.height);
+	popupClosed(): void{
+		if(this.canvas){
+			this.canvas.getContext('2d').clearRect(0, 0, this.canvas.width, this.canvas.height);
+		}
 	}
 
-	private rotateOuter(e){
+	// get rotation angle of outer value based on color
+	private getOuterRotation(): void{
+		// get hsl
+		let hsl: number[] = this.colorSelector('hex', 'hsl', this.color);
+		let deg = (hsl[2] / 100) * 180;
+		this.styles.outerRotation = deg;
+	}
+
+	// get inner position of pointer
+	private getInnerPosition(): void{
+		const hsl: number[] = this.colorSelector('hex', 'hsl', this.color),
+		angle: number = hsl[0] - 135,
+		a: number = angle * 0.0174532925,
+		x: number = Math.round(200 * -Math.sin(a)),
+		y: number = Math.round(200 * -Math.cos(a));
+
+		this.styles.innerTranslate = `translate(${x}%, ${y}%)`;
+	}
+
+	private rotateOuter(e): void{
 		let container = this.container.nativeElement.getBoundingClientRect();
 
 		const center = {
@@ -210,54 +284,69 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 	}
 
 	private moveInner(e){
-		let container = this.canvas.getBoundingClientRect();
+		const container: DOMRect = this.canvas.getBoundingClientRect();
+		const x: number = e.pageX || (e.touches ? e.touches[0].clientX : 0);
+		const y: number = e.pageY || (e.touches ? e.touches[0].clientY : 0);
 
-		const x = e.pageX || (e.touches ? e.touches[0].clientX : 0);
-		const y = e.pageY || (e.touches ? e.touches[0].clientY : 0);
-
-   		const center = {
+		const center: {[key: string]: number} = {
 			x: container.left + (container.width) / 2,
 			y: container.top + (container.height) / 2,
 		};
 
-		let moveX = x - center.x, moveY = y - center.y;
+		let moveX: number = x - center.x, moveY = y - center.y;
 		
 		if (Math.sqrt(Math.pow(Math.abs(x - center.x), 2) + Math.pow(Math.abs(y - center.y), 2)) > container.width / 2) {
-			let ratio = ( (x - container.left) - (container.width / 2))/( (y - container.top) - (container.height / 2));
-			let sign = 1;
+			const ratio: number = ( (x - container.left) - (container.width / 2))/( (y - container.top) - (container.height / 2));
+			let sign: number = 1;
 			if( (x - container.left) - (container.width / 2) < 0){
 				sign = -1;
 			}
 			moveX = Math.sqrt(Math.pow(ratio,2)*Math.pow(container.width,2)/4/(1+Math.pow(ratio,2))) * sign;
 			moveY = (moveX/ratio);
 		}
-		if(this.canvas.getContext('2d')){
-			const pixel = this.canvas.getContext('2d').getImageData(moveX + (container.width / 2), moveY + (container.height / 2), 1, 1).data || null;
+		if(this.canvas.getContext('2d') && !isNaN(moveX) && !isNaN(moveY)){
+			const ctx = this.canvas.getContext('2d');
+			const pixel = ctx.getImageData(moveX + (container.width / 2), moveY + (container.height / 2), 1, 1).data || null;
 			const dColor = pixel[2] + 256 * pixel[1] + 65536 * pixel[0];
-			const hex = ('0000' + dColor.toString(16)).substr(-6);
-			// update 
-			this.color = hex;
+			const hex: string = ('0000' + dColor.toString(16)).substr(-6);
+			// move inner dot
 			this.styles.innerTranslate = `translate(${ moveX }px, ${ moveY }px)`;
-			this.getOuterGradientColor();
-
-			if (this.bool_data_updateOnMove) {
-				this.waitForThreshold += 1;
-				if (this.waitForThreshold === parseInt(this.data_threshold)) {
-					this.sendValue(this.colorSelector('hex', this.arr_data_colorOutput[0], this.color));
-					this.waitForThreshold = 0;
+			// validate hex
+			if(/([0-9A-F]{3}){1,2}$/i.test(hex) && hex !== '00000'){
+				// get hex color based on current brightness
+				const currentHSL: number[] = this.colorSelector('hex', 'hsl', this.color);
+				let moveHSL: number[] = this.colorSelector('hex', 'hsl', hex);
+				// update moving color based on luminance of current color
+				moveHSL[2] = currentHSL[2];
+				// get rgb
+				const rgb: number[] = this.HSLToRGB(moveHSL);
+				const hexMod: string = this.RGBToHex(rgb);
+				// update color
+				this.color = hexMod;
+				// update outer gradient
+				this.getOuterGradientColor();
+				if (this.bool_data_updateOnMove) {
+					this.waitForThreshold += 1;
+					if (this.waitForThreshold === parseInt(this.data_threshold)) {
+						this.sendValue(this.colorSelector('hex', this.arr_data_colorOutput[0], this.color));
+						this.waitForThreshold = 0;
+					}
 				}
 			}
 		}
 	}
 
 	// get the outer gradient color
-	private getOuterGradientColor(){
-		this.styles.outerGradient = this.colorSelector('hex', 'rgb', this.color);
+	private getOuterGradientColor(): void{
+		let hsl: number[] = this.colorSelector('hex', 'hsl', this.color);
+		hsl[2] = 50;
+		const rgb: number[] = this.colorSelector('hsl', 'rgb', hsl);
+		this.styles.outerGradient = 'rgb('+rgb[0]+', '+rgb[1]+', '+rgb[2]+')';
 	}
 
 	// calc lighter/darket color based on percentage
-	private getAdjustedOuterColor(ratio: number){
-		let rgb = this.styles.outerGradient.substring(4, this.styles.outerGradient.length-1).replace(/ /g, '').split(',').map(Number);
+	private getAdjustedOuterColor(ratio: number): number[]{
+		let rgb: number[] = this.styles.outerGradient.substring(4, this.styles.outerGradient.length-1).replace(/ /g, '').split(',').map(Number);
 		if(ratio > 0){
 			// lighten
 			for (let i = 0; i < rgb.length; i++) {
@@ -270,7 +359,6 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 			   rgb[i] = Math.floor(rgb[i] + ratio * rgb[i]);
 			}
 		}
-
 		return rgb;
 	}
 
@@ -283,6 +371,9 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 			if (to === 'rgb') {
 				return this.hexToRGB(value);
 			}
+			if (to === 'hsl'){
+				return this.hexToHSL(value);
+			}
 		}
 		if (from === 'rgb') {
 			if (to === 'hex' || to === '#hex') {
@@ -294,11 +385,16 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 				return value.substr(1);
 			}
 		}
+		if (from === 'hsl'){
+			if (to === 'rgb'){
+				return this.HSLToRGB(value);
+			}
+		}
 	}
 
 	private hexToRGB(hex) {
 		const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	 	return result ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})` : null;
+		return result ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})` : null;
 	}
 
 	private RGBToHex(rgb) {
@@ -308,6 +404,41 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 			const RGB = (rgb.split('(')[1].split(')')[0]).split(',');
 			return ((1 << 24) + (RGB[0] << 16) + (RGB[1] << 8) + RGB[2]).toString(16).slice(1);
 		}
+	}
+
+	private hexToHSL(hex): number[]{
+		let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+		let r = parseInt(result[1], 16);
+		let g = parseInt(result[2], 16);
+		let b = parseInt(result[3], 16);
+
+		r /= 255, g /= 255, b /= 255;
+		let max = Math.max(r, g, b), min = Math.min(r, g, b);
+		let h, s, l = (max + min) / 2;
+		if(max == min){
+			h = s = 0;
+		}else{
+			let d = max - min;
+			s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+			switch(max){
+				case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+				case g: h = (b - r) / d + 2; break;
+				case b: h = (r - g) / d + 4; break;
+			}
+			h /= 6;
+		}
+		s = Math.round(s*100), l = Math.round(l*100), h = Math.round(h*360);
+		return [h,s,l];
+	}
+
+	private HSLToRGB(hsl): number[]{
+		let r, g, b;
+		let h = hsl[0], s = hsl[1] / 100, l = hsl[2] / 100;
+
+		let a = s * Math.min(l,1-l);
+		let f = (n,k=(n+h/30)%12) => l - a * Math.max(Math.min(k-3,9-k,1),-1); 
+		return [Math.round(f(0) * 255), Math.round(f(8) * 255), Math.round(f(4) * 255)]; 
 	}
 
 	private sendValue(val) {
@@ -352,6 +483,10 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 
 	ngOnDestroy(){
 		this.fhem.removeDevice(this.ID);
+		this.selectComponent.removeHandle(this.ID, 'resize');
+		if(!this.bool_data_usePopup){
+			this.popupClosed();
+		}
 	}
 
 	constructor(
@@ -359,7 +494,8 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 		public settings: SettingsService,
 		private storage: StorageService,
 		private ref: ElementRef,
-		private native: NativeFunctionsService) {
+		private native: NativeFunctionsService,
+		private selectComponent: SelectComponentService) {
 	}
 
 	static getSettings() {
@@ -383,6 +519,8 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 				{variable: 'arr_data_animationStyle', default: 'scale,from-top,from-bottom,from-left,from-right,jump-in,flip-in-x,flip-in-y,scale-x,scale-y'},
 				{variable: 'arr_data_colorInput', default: 'hex,#hex,rgb'},
 				{variable: 'arr_data_colorOutput', default: 'hex,#hex,rgb'},
+				{variable: 'bool_data_showFavMenu', default: true},
+				{variable: 'bool_data_usePopup', default: true},
 				{variable: 'bool_data_updateOnMove', default: false},
 				{variable: 'bool_data_customBorder', default: false},
 				{variable: 'bool_data_customAnimation', default: false},
@@ -390,14 +528,19 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 			],
 			dependencies:{
 				data_threshold: { dependOn: 'bool_data_updateOnMove', value: true },
-				data_borderRadius: { dependOn: 'bool_data_customBorder', value: false },
-				data_borderRadiusTopLeft: { dependOn: 'bool_data_customBorder', value: true },
-				data_borderRadiusTopRight: { dependOn: 'bool_data_customBorder', value: true },
-				data_borderRadiusBottomLeft: { dependOn: 'bool_data_customBorder', value: true },
-				data_borderRadiusBottomRight: { dependOn: 'bool_data_customBorder', value: true },
-				bool_data_invertAnimation: { dependOn: 'bool_data_customAnimation', value: true },
-				arr_data_animationStyle: { dependOn: 'bool_data_customAnimation', value: true }
-
+				data_borderRadius: { dependOn: ['bool_data_customBorder', 'bool_data_usePopup'], value: [false, true] },
+				data_borderRadiusTopLeft: { dependOn: ['bool_data_customBorder', 'bool_data_usePopup'], value: [true, true] },
+				data_borderRadiusTopRight: { dependOn: ['bool_data_customBorder', 'bool_data_usePopup'], value: [true, true] },
+				data_borderRadiusBottomLeft: { dependOn: ['bool_data_customBorder', 'bool_data_usePopup'], value: [true, true] },
+				data_borderRadiusBottomRight: { dependOn: ['bool_data_customBorder', 'bool_data_usePopup'], value: [true, true] },
+				bool_data_invertAnimation: { dependOn: ['bool_data_customAnimation', 'bool_data_usePopup'], value: [true, true] },
+				arr_data_animationStyle: { dependOn: ['bool_data_customAnimation', 'bool_data_usePopup'], value: [true, true] },
+				// Dependencies for Colorpicker out of popup
+				data_popupWidth: {dependOn: 'bool_data_usePopup', value: true},
+				data_popupHeight: {dependOn: 'bool_data_usePopup', value: true},
+				data_headline: {dependOn: 'bool_data_usePopup', value: true},
+				bool_data_customBorder: {dependOn: 'bool_data_usePopup', value: true},
+				bool_data_customAnimation: {dependOn: 'bool_data_usePopup', value: true}
 			},
 			dimensions: {minX: 30, minY: 30}
 		};
@@ -406,6 +549,6 @@ export class FhemColorPickerComponent implements OnInit, OnDestroy {
 
 @NgModule({
 	imports: [ComponentsModule, IonicModule, TranslateModule],
-  	declarations: [FhemColorPickerComponent]
+	declarations: [FhemColorPickerComponent]
 })
 class FhemColorPickerComponentModule {}
