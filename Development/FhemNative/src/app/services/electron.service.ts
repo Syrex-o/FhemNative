4399d2e862e7f443f5b5d782a57b35aac22571da
Syrex-o <alberthenkel.ah@gmail.com>
2021-02-08 14:36:20 +0100

    version adjustment to electron 8

--- Development/FhemNative/src/app/services/electron.service.ts
+++ Development/FhemNative/src/app/services/electron.service.ts
@@ -1,27 +1,42 @@
 import { Injectable } from '@angular/core';
 
-import { ipcRenderer, remote, clipboard } from 'electron';
+import * as Electron from 'electron';
+
+interface ElectronWindow extends Window {
+    require(module: string): Electron.RendererInterface;
+}
+
+declare let window: ElectronWindow;
 
 @Injectable({
 	providedIn: 'root'
 })
 
 export class ElectronService {
-	private get electron(): boolean {
-		return !!(window && window.process && window.process.type);
-	}
+	private _electron: Electron.RendererInterface;
+
+	private get electron(): Electron.RendererInterface {
+        if (!this._electron) {
+            if (window && window.require) {
+                this._electron = window.require('electron');
+                return this._electron;
+            }
+            return null;
+        }
+        return this._electron;
+    }
 
 	public get remote(): Electron.Remote {
-		return this.electron ? window.require('electron').remote : null;
-	}
+        return this.electron ? this.electron.remote : null;
+    }
 
 	public get ipcRenderer(): Electron.IpcRenderer {
-		return this.electron ? window.require('electron').ipcRenderer : null;
-	}
+        return this.electron ? this.electron.ipcRenderer : null;
+    }
 
 	public get clipboard(): Electron.Clipboard {
-		return this.electron ? window.require('electron').clipboard : null;
-	}
+        return this.electron ? this.electron.clipboard : null;
+    }
 
 	public get process(): any {
 		return this.remote ? this.remote.process : null;
