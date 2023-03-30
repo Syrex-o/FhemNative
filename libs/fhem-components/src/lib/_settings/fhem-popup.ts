257f1bdc99d7317749302f0e4dee3b990315e262
Syrex-o <alberthenkel.ah@gmail.com>
2023-03-30 14:05:38 +0200

    added popup component

---
+++ libs/fhem-components/src/lib/_settings/fhem-popup.ts
@@ -0,0 +1,50 @@
+import { ComponentSettings } from "@fhem-native/types/components";
+
+/*
+    Popup Container component
+*/
+export const Settings: ComponentSettings = {
+    name: 'Popup',
+	type: 'container',
+	dimensions: {minX: 40, minY: 40},
+	inputs: {
+		data: {
+            device: {type: 'string', value: ''},
+            reading: {type: 'string', value: 'state'},
+            getOn: {type: 'string', value: ''},
+            getOff: {type: 'string', value: ''},
+
+			headline: {type: 'string', value: ''},
+            width: {type: 'number', value: 80},
+			height: {type: 'number', value: 80},
+
+            borderRadius: {type: 'number', value: 5},
+			borderRadiusTopLeft: {type: 'number', value: 5},
+			borderRadiusTopRight: {type: 'number', value: 5},
+			borderRadiusBottomLeft: {type: 'number', value: 5},
+			borderRadiusBottomRight: {type: 'number', value: 5},
+		},
+		icon: {
+            iconOn: 'add-circle',
+            iconOff: 'add-circle',
+        },
+		style: {
+			iconColorOn: '#86d993',
+			iconColorOff: '#86d993',
+			backgroundColorOn: '#303030',
+			backgroundColorOff: '#303030',
+		},
+		bool: {
+			openOnReading: false,
+            customBorder: false
+		}
+	},
+    dependencies: {
+        // custom border
+		'data.borderRadius': {dependOn: ['bool.customBorder'], value: [false]},
+		'data.borderRadiusTopLeft': {dependOn: ['bool.customBorder'], value: [true]},
+		'data.borderRadiusTopRight': {dependOn: ['bool.customBorder'], value: [true]},
+		'data.borderRadiusBottomLeft': {dependOn: ['bool.customBorder'], value: [true]},
+		'data.borderRadiusBottomRight': {dependOn: ['bool.customBorder'], value: [true]},
+    }
+};
---
+++ libs/fhem-components/src/lib/fhem-popup/fhem-popup.component.ts
@@ -0,0 +1,93 @@
+import { Component, Input } from '@angular/core';
+import { map, tap } from 'rxjs';
+
+import { FhemComponentModule } from '../_fhem-component/fhem-component.module';
+import { ComponentLoaderModule, EditButtonComponent, IconModule, PopupComponent } from '@fhem-native/components';
+
+import { EditorService, FhemService, StructureService } from '@fhem-native/services';
+
+import { ComponentPosition, FhemComponentSettings } from '@fhem-native/types/components';
+import { FhemDevice } from '@fhem-native/types/fhem';
+
+@Component({
+	standalone: true,
+	selector: 'fhem-native-component-popup',
+	templateUrl: './fhem-popup.component.html',
+	styleUrls: ['./fhem-popup.component.scss'],
+	imports: [
+		IconModule,
+		PopupComponent,
+		FhemComponentModule,
+		EditButtonComponent,
+        ComponentLoaderModule
+	]
+})
+export class FhemPopupComponent{
+	// meta
+	@Input() UID!: string;
+	@Input() position!: ComponentPosition;
+	
+	// Data
+    @Input() device!: string;
+    @Input() reading!: string;
+    @Input() getOn!: string;
+    @Input() getOff!: string;
+
+	@Input() headline!: string;
+	@Input() width!: number;
+	@Input() height!: number;
+
+	@Input() borderRadius!: number;
+	@Input() borderRadiusTopLeft!: number;
+	@Input() borderRadiusTopRight!: number;
+	@Input() borderRadiusBottomLeft!: number;
+	@Input() borderRadiusBottomRight!: number;
+
+    // Icons
+    @Input() iconOn!: string;
+    @Input() iconOff!: string;
+
+	// Styling
+	@Input() iconColorOn!: string;
+	@Input() iconColorOff!: string;
+    @Input() backgroundColorOn!: string;
+	@Input() backgroundColorOff!: string;
+
+	// Bool
+	@Input() openOnReading!: boolean;
+	@Input() customBorder!: boolean;
+
+    popupState = false;
+	buttonState = false;
+
+	// component reference
+	component: FhemComponentSettings|undefined;
+	showEditBtn$ = this.editor.core.getMode().pipe( 
+		tap(()=> this.getContainer()),
+		map(x=>{
+			if( !this.component || this.component.components === undefined ) return false;
+			return x.edit && x.editFrom !== this.component.components[0].containerUID
+		})
+	);
+
+	constructor(private fhem: FhemService, private editor: EditorService, private structure: StructureService){}
+
+	getContainer(): void{
+		// get component for container creation
+		const component = this.structure.getComponent(this.UID);
+		if(component && component.components) this.component = (component as FhemComponentSettings);
+	}
+
+    setFhemDevice(device: FhemDevice): void{
+		this.buttonState = this.fhem.deviceReadingActive(device, this.reading, this.getOn);
+		if(this.openOnReading) this.popupState = this.buttonState;
+    }
+
+    switchToEditMode(): void{
+		if(this.component && this.component.components) this.editor.core.enterEditMode(this.component.components[0].containerUID);
+    }
+
+    togglePpopup(): void{
+		this.popupState = !this.popupState;
+    }
+}
