ee4b103cd70d2a4d1869f6dcbdcd0de946f144bd
Syrex-o <alberthenkel.ah@gmail.com>
2023-04-20 09:40:41 +0200

    added custom color save mechanism

--- libs/services/src/lib/color.service.ts
+++ libs/services/src/lib/color.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { ComponentColors } from '@fhem-native/app-config';
 import { StorageService } from './storage.service';
+import { isValidHex } from '@fhem-native/utils';
 
 @Injectable({providedIn: 'root'})
 export class ColorService {
@@ -19,5 +20,22 @@ export class ColorService {
         
         // combine colors to single array
         this.componentColors = this.componentColors.concat(this.customColors);
+    }
+
+    /**
+     * Add new color to existing list
+     * @param colorVal 
+     * @returns 
+     */
+    public addColor(colorVal: string): void{
+        if(!isValidHex(colorVal)) return;
+
+        if(this.customColors.includes(colorVal)) return;
+        if(this.componentColors.includes(colorVal)) return;
+
+        this.customColors.push(colorVal);
+        this.componentColors.push(colorVal);
+
+        this.storage.changeSetting({name: 'customColors', change: JSON.stringify(this.customColors)});
     }
 }
