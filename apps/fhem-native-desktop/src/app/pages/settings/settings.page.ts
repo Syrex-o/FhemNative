1902026b109ea00573a7f96068dd6dd499e66dac
Syrex-o <alberthenkel.ah@gmail.com>
2023-04-20 09:38:10 +0200

    updated desktop pages to standalone

--- apps/fhem-native-desktop/src/app/pages/settings/settings.page.ts
+++ apps/fhem-native-desktop/src/app/pages/settings/settings.page.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { Route } from '@angular/router';
+
+import { SettingsPageModule } from '@fhem-native/pages';
 
 @Component({
+	standalone: true,
 	selector: 'fhem-native-desktop-settings',
-	templateUrl: 'settings.page.html'
+	templateUrl: 'settings.page.html',
+    imports: [
+		SettingsPageModule
+	]
 })
 export class DesktopSettingsPageComponent{
     
 }
+
+export const DESKTOP_SETTINGS_ROUTES: Route[] = [
+    {
+        path: '',
+        component: DesktopSettingsPageComponent
+    }
+];
