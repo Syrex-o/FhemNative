0070e59cc9387c2a885948eb603646ebf13ab03a
Syrex-o <alberthenkel.ah@gmail.com>
2023-04-28 10:42:34 +0200

    new logic of back button service

--- libs/services/src/lib/back-button.service.ts
+++ libs/services/src/lib/back-button.service.ts
@@ -1,35 +1,58 @@
-import { Injectable } from '@angular/core';
-import { Platform } from '@ionic/angular';
-import { Subscription } from 'rxjs';
+import { Injectable, Inject } from '@angular/core';
+
+import { APP_CONFIG } from '@fhem-native/app-config';
+
+import { App } from '@capacitor/app';
+
+interface BackBtnSub {
+	handleID: string,
+	callback: ()=> void,
+	removeOnCallback: boolean
+}
 
 @Injectable({providedIn: 'root'})
 export class BackButtonService {
-	private subs: Array<{ priority: number, handleID: string, callback: any, sub?: Subscription }> = [];
+	private subs: BackBtnSub[] = [];
 
-	constructor(private platform: Platform){}
+	constructor(@Inject(APP_CONFIG) private appConfig: any){
+		// back button listener
+		if(this.appConfig.platform === 'mobile') App.addListener('backButton', ({canGoBack})=> this.handleBackBtn(canGoBack) );
+	}
 
-	public handle(handleID: string, callback: any): void{
-		// determine priority
-		const priority = this.subs.length > 0 ? this.subs.length + 1 : 1;
-		// Add to stack
-		this.subs.push({priority: priority, handleID: handleID, callback: callback});
-		// Assing Native Back button event
-		this.subs[this.subs.length - 1]['sub'] = this.platform.backButton.subscribeWithPriority(priority, () => {
-			this.subs[this.subs.length - 1].callback();
-		});
+	private async handleBackBtn(canGoBack: boolean): Promise<void>{
+		// default behaviour
+		if(this.subs.length === 0){
+			if(canGoBack) return window.history.back();
+			return App.exitApp();
+		}
+
+		// callback of custom logic
+		const relevantSub = this.subs[this.subs.length -1];
+		if(!relevantSub) return;
+
+		relevantSub.callback();
+		if(relevantSub.removeOnCallback) this.removeHandle(relevantSub.handleID);
+	}
+
+	public handle(handleID: string, callback: ()=> void, removeOnCallback?: boolean): void{
+		// only add handles for mobile
+		if(this.appConfig.platform !== 'mobile') return;
+
+		removeOnCallback = removeOnCallback || false;
+
+		const existingSub = this.subs.find(x=> x.handleID === handleID);
+		if(existingSub){
+			existingSub.callback = callback;
+			existingSub.removeOnCallback = removeOnCallback;
+			return;
+		}
+
+		this.subs.push({handleID, callback, removeOnCallback});
 	}
 
 	// remove a handle
 	public removeHandle(handleID: string): void{
 		const index: number = this.subs.findIndex(x => x.handleID === handleID);
-		if(index > -1){
-			const sub: Subscription|undefined = this.subs[index].sub;
-			if(sub){
-				// remove listener
-				sub.unsubscribe();
-			}
-			// remove sub from stack
-			this.subs.splice(index, 1);
-		}
+		if(index > -1) this.subs.splice(index, 1);
 	}
 }
