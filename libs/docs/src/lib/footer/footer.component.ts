c9b2e3e93fb37fbc0f8794f5e6f135bb223bceb8
Syrex-o <alberthenkel.ah@gmail.com>
2023-04-28 10:31:48 +0200

    added docs footer component

---
+++ libs/docs/src/lib/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+    standalone: true,
+	selector: 'fhem-native-doc-footer',
+	templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss'],
+    imports: [ IonicModule, RouterModule, TranslateModule ]
+})
+export class DocFooterComponent{
+
+}
