355e99cef2be6b4e618e433b7e5d97c0e68f7254
Syrex-o <alberthenkel.ah@gmail.com>
2023-04-29 11:03:53 +0200

    updated capacitor config

--- apps/fhem-native/capacitor.config.ts
+++ apps/fhem-native/capacitor.config.ts
@@ -1,10 +1,19 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'de.slapapps.fhemnative',
-  appName: 'FhemNative',
-  webDir: '../../dist/apps/fhem-native',
-  bundledWebRuntime: false,
+	appId: 'de.slapapps.fhemnative',
+	appName: 'FhemNative',
+	webDir: '../../dist/apps/fhem-native',
+	bundledWebRuntime: false,
+	plugins: {
+		SplashScreen: {
+			launchAutoHide: false,
+			splashImmersive: false,
+			splashFullScreen: false,
+			backgroundColor: '#f9fafe',
+			androidScaleType: 'CENTER_CROP'
+		}
+	},
 };
 
 export default config;
