ca60dd34e474412c8b0202a29324c74054401b8d
Syrex-o <alberthenkel.ah@gmail.com>
2023-04-30 10:08:24 +0200

    update version codes to 4.0.2

--- libs/app-config/version.ts
+++ libs/app-config/version.ts
@@ -2,11 +2,11 @@
  * Main version code
  */
 export const MobileVersionCode: Version = {
-	major: 4, minor: 0, patch: 0
+	major: 4, minor: 0, patch: 2
 }
 
 export const DesktopVersionCode: Version = {
-	major: 4, minor: 0, patch: 0
+	major: 4, minor: 0, patch: 2
 }
 
 export interface Version {
