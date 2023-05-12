e24dbe2249760ba36b6863c6e6bbfbd62ae40aa4
Syrex-o <alberthenkel.ah@gmail.com>
2023-05-12 13:33:18 +0200

    added theme change to docs

--- libs/docs/src/lib/header/header.component.ts
+++ libs/docs/src/lib/header/header.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
+import { SettingsService, StorageService, ThemeService } from '@fhem-native/services';
+
+import { ThemeName } from '@fhem-native/types/common';
 
 @Component({
     standalone: true,
 	selector: 'fhem-native-doc-header',
 	templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
-    imports: [
-        IonicModule
-    ]
+    imports: [ IonicModule ]
 })
 export class DocHeaderComponent{
+    constructor(
+        private theme: ThemeService,
+        private storage: StorageService,
+        public settings: SettingsService){
+    }
 
+    changeTheme(): void{
+        const changeTo: ThemeName = this.settings.app.theme === 'dark' ? 'bright' : 'dark';
+        this.storage.changeSetting({name: 'theme', change: changeTo}).then((res: any)=>{ this.settings.app.theme = res; });
+        this.theme.changeTheme(changeTo);
+    }
 }
