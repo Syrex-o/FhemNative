bf41e051454d431400fa84f172b0f0acc23f4afc
Syrex-o <alberthenkel.ah@gmail.com>
2023-05-16 14:06:45 +0200

    add text styling options + theme color as default

--- libs/fhem-components/src/lib/_settings/fhem-html.ts
+++ libs/fhem-components/src/lib/_settings/fhem-html.ts
@@ -1,17 +1,33 @@
 import { ComponentSettings } from "@fhem-native/types/components";
+import { componentPositionStyles, componentTextStyles } from "./common";
 
 /*
-    HTML component
+	HTML component
 */
 export const Settings: ComponentSettings = {
-    name: 'Html',
+	name: 'Html',
 	type: 'fhem',
 	dimensions: {minX: 30, minY: 30},
 	inputs: {
 		data: {
 			device: {type: 'string', value: ''},
-            reading: {type: 'string', value: 'state'},
-            rawHtml: {type: 'string', value: ''}
+			reading: {type: 'string', value: 'state'},
+			rawHtml: {type: 'string', value: ''}
+		},
+		arr_data: {
+			textStyle: {items: componentTextStyles, value: 'normal'},
+			textAlign: {items: componentPositionStyles, value: 'left'}
+		},
+		style: {
+			textColor: '#86d993'
+		},
+		bool: {
+			customTextProperties: false
 		}
+	},
+	dependencies: {
+		'style.textColor': {dependOn: ['bool.customTextProperties'], value: [true]},
+		'arr_data.textStyle': {dependOn: ['bool.customTextProperties'], value: [true]},
+		'arr_data.textAlign': {dependOn: ['bool.customTextProperties'], value: [true]},
 	}
 };
--- libs/fhem-components/src/lib/fhem-html/fhem-html.component.ts
+++ libs/fhem-components/src/lib/fhem-html/fhem-html.component.ts
@@ -2,40 +2,50 @@ import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/cor
 
 import { FhemComponentModule } from '../_fhem-component/fhem-component.module';
 
-import { ComponentPosition } from '@fhem-native/types/components';
 import { TextBlockModule } from '@fhem-native/components';
+import { TextPosition, TextStyle, getFontStyleFromSelection, getFontWeightFromSelection } from '@fhem-native/utils';
+
 import { FhemDevice } from '@fhem-native/types/fhem';
+import { ComponentPosition } from '@fhem-native/types/components';
+import { ThemeService } from '@fhem-native/services';
 
 @Component({
 	standalone: true,
 	selector: 'fhem-native-component-html',
 	template: `
-        <fhem-native-component 
-            [UID]="UID" 
-            [position]="position"
-            [minDimensions]="{width: 30, height: 30}"
-            [fhemDeviceConfig]="{
-                device, reading,
-                connected: rawHtml === '' ? true : false,
-                readingAvailable: (device !== '' && reading !== '') ? true : false
-            }"
-            (initComponent)="onInitComponent()"
-            (initDevice)="setFhemDevice($event)"
-            (updateDevice)="setFhemDevice($event)">
-            <div class="fhem-native-html" [ngClass]="htmlText ? 'present' : 'error'"  #HTML>
-                <!-- no html available -->
-                <fhem-native-text-block *ngIf="!htmlText"
-                    [label]="('COMPONENTS.Html.ERRORS.NO_HTML.name' | translate)"
-                    [info]="('COMPONENTS.Html.ERRORS.NO_HTML.info' | translate)">
-                </fhem-native-text-block>
-            </div>
-        </fhem-native-component>
-    `,
+		<fhem-native-component 
+			[UID]="UID" 
+			[position]="position"
+			[minDimensions]="{width: 30, height: 30}"
+			[fhemDeviceConfig]="{
+				device, reading,
+				connected: rawHtml === '' ? true : false,
+				readingAvailable: (device !== '' && reading !== '') ? true : false
+			}"
+			(initComponent)="onInitComponent()"
+			(initDevice)="setFhemDevice($event)"
+			(updateDevice)="setFhemDevice($event)">
+			<div #HTML class="fhem-native-html font-b"
+				[ngClass]="htmlText ? 'present' : 'error'"
+				[ngStyle]="{
+					'font-weight': labelFontWeight,
+					'font-style': labelFontStyle,
+					'text-align': customTextProperties ? textAlign : 'left',
+					'color': customTextProperties ? textColor : (textColor$ | async)
+				}">
+				<!-- no html available -->
+				<fhem-native-text-block *ngIf="!htmlText"
+					[label]="('COMPONENTS.Html.ERRORS.NO_HTML.name' | translate)"
+					[info]="('COMPONENTS.Html.ERRORS.NO_HTML.info' | translate)">
+				</fhem-native-text-block>
+			</div>
+		</fhem-native-component>
+	`,
 	styleUrls: ['./fhem-html.component.scss'],
 	imports: [FhemComponentModule, TextBlockModule]
 })
 export class FhemHtmlComponent{
-    @ViewChild('HTML', { read: ElementRef, static: false }) htmlContainer: ElementRef|undefined;
+	@ViewChild('HTML', { read: ElementRef, static: false }) htmlContainer: ElementRef|undefined;
 
 	// meta
 	@Input() UID!: string;
@@ -43,33 +53,54 @@ export class FhemHtmlComponent{
 	
 	// Data
 	@Input() device!: string;
-    @Input() reading!: string;
-    @Input() rawHtml!: string
+	@Input() reading!: string;
+	@Input() rawHtml!: string
 
-    htmlText: string|undefined;
+	// Selections
+	@Input() textStyle!: TextStyle;
+	@Input() textAlign!: TextPosition;
+
+	// Styling
+	@Input() textColor!: string;
+
+	// Bool
+	@Input() customTextProperties!: boolean;
+
+	// text style
+	labelFontWeight = 400;
+	labelFontStyle = 'normal';
+	textColor$ = this.theme.getThemePipe('--text-a');
+
+	htmlText: string|undefined;
 	private rendererEl: HTMLElement|undefined;
 
-    constructor(private renderer: Renderer2){}
+	constructor(private renderer: Renderer2, private theme: ThemeService){}
 
-    onInitComponent(): void{
-        if(this.rawHtml !== '') {
-            this.htmlText = this.rawHtml;
-            this.insertHtml();
-        }
-    }
+	onInitComponent(): void{
+		if(this.rawHtml !== '') {
+			this.htmlText = this.rawHtml;
+			this.insertHtml();
+		}
 
-    setFhemDevice(device: FhemDevice): void{
-        if(device && device.readings[this.reading]){
-            this.htmlText =  device.readings[this.reading].Value;
-            this.insertHtml();
-        }
-    }
+		if(this.customTextProperties){
+			this.labelFontStyle = getFontStyleFromSelection(this.textStyle);
+			this.labelFontWeight = getFontWeightFromSelection(this.textStyle);
+		}
+	}
 
-    private insertHtml(): void{
-        if(this.rendererEl && this.htmlContainer) this.renderer.removeChild(this.htmlContainer, this.rendererEl);
-        // create elem
-        this.rendererEl = this.renderer.createElement('div');
-        this.renderer.appendChild(this.htmlContainer?.nativeElement, this.rendererEl);
+	setFhemDevice(device: FhemDevice): void{
+		if(device && device.readings[this.reading]){
+			this.htmlText = device.readings[this.reading].Value;
+			this.insertHtml();
+		}
+	}
+
+	private insertHtml(): void{
+		if(this.rendererEl && this.htmlContainer) this.renderer.removeChild(this.htmlContainer, this.rendererEl);
+		// create elem
+		this.rendererEl = this.renderer.createElement('div');
+		// add to view
+		this.renderer.appendChild(this.htmlContainer?.nativeElement, this.rendererEl);
 		this.renderer.setProperty(this.rendererEl, 'innerHTML', this.htmlText);
-    }
+	}
 }
