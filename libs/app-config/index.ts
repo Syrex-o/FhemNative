4a355dd1e27a100caf094b4079e094ca514ec9d1
Syrex-o <alberthenkel.ah@gmail.com>
2023-08-09 09:02:38 +0200

    interface for APP_CONFIG

--- libs/app-config/index.ts
+++ libs/app-config/index.ts
@@ -1,7 +1,16 @@
 import { InjectionToken } from '@angular/core';
+import { Version } from './version';
 
 export * from './properties';
 export * from './fhem-components';
 export * from './version';
 
-export const APP_CONFIG = new InjectionToken('Application config');
+export interface AppConfig {
+    production: boolean,
+    platform: Platform,
+    versionCode: Version
+}
+
+export type Platform = 'mobile'|'desktop';
+
+export const APP_CONFIG = new InjectionToken<AppConfig>('Application config');
