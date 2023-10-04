11dc42c09c2845a26f83938f6d39f5c4c208e06d
Syrex-o <alberthenkel.ah@gmail.com>
2023-10-04 12:59:34 +0200

    update versions to 4.3.3

--- libs/app-config/version.ts
+++ libs/app-config/version.ts
@@ -2,11 +2,11 @@
  * Main version code
  */
 export const MobileVersionCode: Version = {
-	major: 4, minor: 3, patch: 2
+	major: 4, minor: 3, patch: 3
 }
 
 export const DesktopVersionCode: Version = {
-	major: 4, minor: 3, patch: 2
+	major: 4, minor: 3, patch: 3
 }
 
 export interface Version {
