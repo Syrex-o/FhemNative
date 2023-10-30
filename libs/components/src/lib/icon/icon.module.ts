a9cfd5a84151ca8ef7e8805df19ba5bf00dbbf8d
Syrex-o <alberthenkel.ah@gmail.com>
2023-10-30 18:03:08 +0100

    add MDI Icons

--- libs/components/src/lib/icon/icon.module.ts
+++ libs/components/src/lib/icon/icon.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
+
 import { CoreModule } from '@fhem-native/modules';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatIconRegistry, MatIconModule } from '@angular/material/icon';
 
 import { IconComponent } from './icon.component';
 
 @NgModule({
-	imports: [ CoreModule, FontAwesomeModule ],
+	imports: [ 
+		CoreModule,
+		MatIconModule,
+		HttpClientModule,
+		FontAwesomeModule
+	],
     declarations: [ IconComponent ],
 	exports: [ IconComponent ]
 })
-export class IconModule {}
+export class IconModule {
+	constructor(matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer){
+		matIconRegistry.addSvgIconSet( domSanitizer.bypassSecurityTrustResourceUrl('./assets/mdi.svg') );
+	}
+}
