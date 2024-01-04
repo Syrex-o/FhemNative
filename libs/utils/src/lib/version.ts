7422ca6c394368ad293456db74be9288ec0e4ea1
Syrex-o <alberthenkel.ah@gmail.com>
2024-01-04 16:01:08 +0100

    add version helper

--- libs/utils/src/lib/version.ts
+++ libs/utils/src/lib/version.ts
@@ -19,3 +19,20 @@ export function getRawVersionCode(versionCode: Version): string {
 export function getTranslatedVersionCode(versionCode: Version): number{
     return versionCode.major * 10000 + versionCode.minor * 1000 + versionCode.patch * 100;
 }
+
+/**
+ * raw version code string to formatted version definition
+ * @param versionCode Exp. '1.0.0'
+ * @returns formatted version definition
+ */
+export function versionCodeToVersion(versionCode: string): Version {
+    const splitVersion = versionCode.split('.');
+
+    const vCode = {
+        major: parseInt(splitVersion[0]) || 0, 
+        minor: parseInt(splitVersion[1]) || 0, 
+        patch: parseInt(splitVersion[2]) || 0
+    };
+
+    return vCode;
+}
